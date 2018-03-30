6b7b2ed3d407926bcd2b800cb0078d828123187a
zander312 <iskander93@gmail.com>
2018-03-30 21:06:13 +0000

    switch out test clusters for HDP05 and HDP06 clusters

--- BigDataOrchestrator/BigDataOrchestrator/index.js
+++ BigDataOrchestrator/BigDataOrchestrator/index.js
@@ -92,7 +92,7 @@ const startTaxi = () => {
     })
 }
 
-// start hgos cluster (test)
+// start taxi cluster
 const startHgos = () => {
   ec2.startInstances({
     InstanceIds: clusterMap["4734113"],
@@ -104,12 +104,30 @@ const startHgos = () => {
       console.log(data.StartingInstances[0].CurrentState)
     }
   })
+  axios({
+      url: `http://${ENV.parsed.HGOS_HOSTNAME}:8080/api/v1/clusters/${ENV.parsed.HGOS_CLUSTERNAME}/services`,
+      method: 'put',
+      headers: { 'X-Requested-By': 'ambari' },
+      auth: {
+        username: ENV.parsed.AMBARI_USER,
+        password: ENV.parsed.AMBARI_PASS
+      },
+      data: `{"RequestInfo":{"context":"_PARSE_.START.ALL_SERVICES","operation_level":{"level":"CLUSTER","cluster_name":"${ENV.parsed.HGOS_CLUSTERNAME}"}},"Body":{"ServiceInfo":{"state":"STARTED"}}}`
+    })
+    .then((res) => {
+      console.log("starting hgos services")
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.log("error starting hgos services")
+      console.log(err.response.data)
+    })
 }
 
 // cluster id: [instances in cluster]
 const clusterMap = {
-  "4734111": ["i-0ee369bc32405a067", "i-01974cd0004d386f0", "i-0a71c821deec99cab", "i-0aa503cf730e2b848", "i-0ed1b109bcfe83a8c"],
-  "4734113": ["i-0f317179009ab8bad"]
+  "4734111": ["i-0ff97b10c21d2ab14", "i-0f7943945cea2cef1", "i-00bbc8fb43adbcc76", "i-018a42946e8afff32", "i-04ef2eee18b4f85d5", "i-05423cdc919b8ea75", "i-07fd7510b086d7189", "i-0afd9479aa4df26fc", "i-0b86ce6cd05150122", "i-009f478c4a4d6c89f"],
+  "4734113": ["i-054d1fb29655dcc7e", "i-0bfe8524f296ab067", "i-0438c70e68474d6d5", "i-0009679e06735dec5", "i-05f8cc815a420bca6", "i-0f9471f85c1a01b66", "i-0fad567df991f235c"]
 }
 
 // payloads
